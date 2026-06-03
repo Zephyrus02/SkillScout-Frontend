dd1d059b3f02fa1119bb4df15fa6f7e5358dd855
Aneesh Raskar <aneeshraskar@gmail.com>
2026-06-03 09:30:49 +0530

    integrated agentation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { AuthProvider } from "@/contexts/AuthContext";
+
+const Agentation = dynamic(
+  () => import("agentation").then((module) => module.Agentation),
+  { ssr: false },
+);
 
 const inter = Inter({
   subsets: ["latin"],
@@ -13,11 +19,17 @@ const inter = Inter({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const agentationEndpoint =
+    process.env.NEXT_PUBLIC_AGENTATION_ENDPOINT || "http://localhost:4747";
+
   return (
     <div className={`${inter.variable} font-sans`}>
       <ThemeProvider>
         <AuthProvider>
           <Component {...pageProps} />
+          {process.env.NODE_ENV === "development" && (
+            <Agentation endpoint={agentationEndpoint} />
+          )}
           <Toaster richColors position="top-right" closeButton />
         </AuthProvider>
       </ThemeProvider>
